97d60b3c1ff9a32fc3f20432558a0fd662eb1c70
tdog9 <hearn.tenny@icloud.com>
2026-04-02 21:42:41 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// VeloForge Service Worker v2
-const CACHE_NAME = 'veloforge-v20';
+\// VeloForge Service Worker v2
+const CACHE_NAME = 'veloforge-v21';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
